14e26ccad2f089a145c5d85af2ba43113ae16930
Johan_Valencia <75028152+Johansvg@users.noreply.github.com>
2024-06-21 20:23:46 -0500

    carga inicial del proyecto

---
+++ src/main/java/com/project/paiweb/PaiwebApplication.java
@@ -0,0 +1,13 @@
+package com.project.paiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiwebApplication.class, args);
+	}
+
+}
